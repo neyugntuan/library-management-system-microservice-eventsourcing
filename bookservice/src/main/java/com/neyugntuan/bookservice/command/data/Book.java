40bef64d644293f64788ba9496a564ae91c87c77
tuannguyenthanh1120 <tuannguyenthanh.work@gmail.com>
2026-08-01 17:14:15 +0700

    create books table

---
+++ bookservice/src/main/java/com/neyugntuan/bookservice/command/data/Book.java
@@ -0,0 +1,26 @@
+package com.neyugntuan.bookservice.command.data;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="books")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue( strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private String author;
+    private boolean isReady;
+
+
+}
