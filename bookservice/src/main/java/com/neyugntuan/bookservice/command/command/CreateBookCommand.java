206ea41608884a86f7d64b52f880d015c8bd65b0
tuannguyenthanh1120 <tuannguyenthanh.work@gmail.com>
2026-08-03 16:21:15 +0700

    dispatch first command

--- bookservice/src/main/java/com/neyugntuan/bookservice/command/command/CreateBookCommand.java
+++ bookservice/src/main/java/com/neyugntuan/bookservice/command/command/CreateBookCommand.java
@@ -1,26 +1,22 @@
-package com.neyugntuan.bookservice.command.data;
+package com.neyugntuan.bookservice.command.command;
 
-
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
-@Entity
-@Table(name ="books")
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Book {
-    @Id
-    @GeneratedValue( strategy = GenerationType.UUID)
+public class CreateBookCommand {
+
+    @TargetAggregateIdentifier
     private String id;
 
     private String name;
     private String author;
     private Boolean isReady;
-
-
 }
