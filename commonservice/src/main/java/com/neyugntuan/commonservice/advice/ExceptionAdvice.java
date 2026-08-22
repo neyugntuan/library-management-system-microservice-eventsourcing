5f0e381bcf3b275be32239dbc38f5a548bc10f71
neyugntuan <neyugntuana9@gmail.com>
2026-08-23 00:59:22 +0700

    add validation request bodt

--- commonservice/src/main/java/com/neyugntuan/commonservice/advice/ExceptionAdvice.java
+++ commonservice/src/main/java/com/neyugntuan/commonservice/advice/ExceptionAdvice.java
@@ -3,13 +3,34 @@ package com.neyugntuan.commonservice.advice;
 import com.neyugntuan.commonservice.model.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName,message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessage> handleException(Exception ex){
         return new ResponseEntity(new ErrorMessage("9999", ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR); //body, code
     }
