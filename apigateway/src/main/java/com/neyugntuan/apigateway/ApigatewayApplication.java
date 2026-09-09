e2075d75f44560a0eb15217fab4f52624c9543b1
neyugntuan <neyugntuana9@gmail.com>
2026-09-09 22:54:24 +0700

    add rate limiter

--- apigateway/src/main/java/com/neyugntuan/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/neyugntuan/apigateway/ApigatewayApplication.java
@@ -3,6 +3,9 @@ package com.neyugntuan.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,5 +14,10 @@ public class ApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
+
+	@Bean
+	public KeyResolver keyResolver(){
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}//phan biet nguoi dung bang dia chi ip
 
 }
