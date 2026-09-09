4fff996435423716c65902036dec7c0362e7bb73
neyugntuan <neyugntuana9@gmail.com>
2026-09-09 21:43:08 +0700

    add api gateway and load balancing

--- employeeservice/src/main/java/com/neyugntuan/employeeservice/query/controller/EmployeeQueryController.java
+++ employeeservice/src/main/java/com/neyugntuan/employeeservice/query/controller/EmployeeQueryController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ import java.util.List;
 @RequestMapping("/api/v1/employees")
 @Tag(name = "Employee Query")
 @Hidden
+@Slf4j
 public class EmployeeQueryController {
     @Autowired
     private QueryGateway queryGateway;
@@ -40,6 +42,7 @@ public class EmployeeQueryController {
     )
     @GetMapping
     public List<EmployeeResponseModel> getAllEmployee(@RequestParam(required = false, defaultValue = "false") Boolean isDisciplined){
+        log.info("Calling to getAllEmployee");
         return queryGateway
                 .query(new GetAllEmployeeQuery(isDisciplined), ResponseTypes.multipleInstancesOf(EmployeeResponseModel.class))
                 .join(); // join để chương trình chờ kết quả
